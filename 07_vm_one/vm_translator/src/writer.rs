a6972eca48d5675e2b0403bd2a0f8278edebacb9
Austin Tindle <tindleaj@gmail.com>
2020-02-22 17:28:06 -0700

    Add push/pop to predifined segments

--- 07_vm_one/vm_translator/src/writer.rs
+++ 07_vm_one/vm_translator/src/writer.rs
@@ -20,6 +20,29 @@ impl Writer {
     self.writeln("@SP");
     self.writeln("M=M+1");
   }
+  /// Writes ASM to self.output that decrements the stack pointer by 1
+  fn write_dec_sp(&mut self) {
+    self.writeln("@SP");
+    self.writeln("M=M-1");
+  }
+
+  /// Writes ASM to self.output that sets the value of *SP to the contents of the D register
+  fn write_dreg_to_stack(&mut self) {
+    self.writeln("@SP");
+    self.writeln("A=M");
+    self.writeln("M=D");
+  }
+
+  /// Copies top item on stack to address value at register provided
+  fn write_copy_stack_head_indirect(&mut self, register: &str) {
+    // Copy SP-1 into *R13
+    self.writeln("@SP");
+    self.writeln("A=M-1");
+    self.writeln("D=M");
+    self.writeln(&format!("{}", register));
+    self.writeln("A=M");
+    self.writeln("M=D");
+  }
 
   pub fn write_math(&mut self, command: MathCommand) {
     use MathCommand::*;
@@ -345,11 +368,29 @@ impl Writer {
     match command {
       Push => match segment {
         Argument => {
-          self.writeln(&format!("push arg {}", index));
+          self.writeln(&format!("// push arg {}", index));
+          // set temp register to index
+          self.writeln(&format!("@{}", index));
+          self.writeln("D=A");
           self.writeln("@ARG");
-          self.writeln(&format!("D=M+{}", index))
+          self.writeln("A=D+M");
+          self.writeln("D=M");
+
+          self.write_dreg_to_stack();
+          self.write_inc_sp();
         }
-        Local => unimplemented!(),
+        Local => {
+          self.writeln(&format!("// push local {}", index));
+          // set temp register to index
+          self.writeln(&format!("@{}", index));
+          self.writeln("D=A");
+          self.writeln("@LCL");
+          self.writeln("A=D+M");
+          self.writeln("D=M");
+
+          self.write_dreg_to_stack();
+          self.write_inc_sp();
+        }
         Static => unimplemented!(),
         Constant => self.writeln(&format!(
           "// push constant {index}
@@ -367,19 +408,113 @@ impl Writer {
           M=M+1",
           index = index,
         )),
-        This => unimplemented!(),
-        That => unimplemented!(),
-        Pointer => unimplemented!(),
-        Temp => unimplemented!(),
+        This => {
+          self.writeln(&format!("// push this {}", index));
+          // set temp register to index
+          self.writeln(&format!("@{}", index));
+          self.writeln("D=A");
+          self.writeln("@THIS");
+          self.writeln("A=D+M");
+          self.writeln("D=M");
+
+          self.write_dreg_to_stack();
+          self.write_inc_sp();
+        }
+        That => {
+          self.writeln(&format!("// push that {}", index));
+          // set temp register to index
+          self.writeln(&format!("@{}", index));
+          self.writeln("D=A");
+          self.writeln("@THAT");
+          self.writeln("A=D+M");
+          self.writeln("D=M");
+
+          self.write_dreg_to_stack();
+          self.write_inc_sp();
+        }
+        Pointer => {
+          self.writeln(&format!("// push pointer {}", index));
+          self.writeln(&format!("@{}", index));
+          self.writeln("D=A");
+          self.writeln("@3"); // pointer base
+          self.writeln("A=D+A");
+          self.writeln("D=M");
+
+          self.write_dreg_to_stack();
+          self.write_inc_sp();
+        }
+        Temp => {
+          self.writeln(&format!("// push temp {}", index));
+          self.writeln(&format!("@{}", index));
+          self.writeln("D=A");
+          self.writeln("@5"); // temp base
+          self.writeln("A=D+A");
+          self.writeln("D=M");
+
+          self.write_dreg_to_stack();
+          self.write_inc_sp();
+        }
       },
 
       Pop => match segment {
-        Argument => unimplemented!(),
-        Local => unimplemented!(),
+        Argument => {
+          self.writeln(&format!("// pop argument {}", index));
+          // set R13 to ARG+index
+          self.writeln(&format!("@{}", index));
+          self.writeln("D=A");
+          self.writeln("@ARG");
+          self.writeln("D=D+M");
+          self.writeln("@R13");
+          self.writeln("M=D");
+
+          self.write_copy_stack_head_indirect("@R13");
+
+          self.write_dec_sp();
+        }
+        Local => {
+          self.writeln(&format!("// pop local {}", index));
+          // set R13 to LCL+index
+          self.writeln(&format!("@{}", index));
+          self.writeln("D=A");
+          self.writeln("@LCL");
+          self.writeln("D=D+M");
+          self.writeln("@R13");
+          self.writeln("M=D");
+
+          self.write_copy_stack_head_indirect("@R13");
+
+          self.write_dec_sp();
+        }
         Static => unimplemented!(),
         Constant => panic!("cannot pop to the constant segment"),
-        This => unimplemented!(),
-        That => unimplemented!(),
+        This => {
+          self.writeln(&format!("// pop this {}", index));
+          // set R13 to THIS+index
+          self.writeln(&format!("@{}", index));
+          self.writeln("D=A");
+          self.writeln("@THIS");
+          self.writeln("D=D+M");
+          self.writeln("@R13");
+          self.writeln("M=D");
+
+          self.write_copy_stack_head_indirect("@R13");
+
+          self.write_dec_sp();
+        }
+        That => {
+          self.writeln(&format!("// pop that {}", index));
+          // set R13 to THAT+index
+          self.writeln(&format!("@{}", index));
+          self.writeln("D=A");
+          self.writeln("@THAT");
+          self.writeln("D=D+M");
+          self.writeln("@R13");
+          self.writeln("M=D");
+
+          self.write_copy_stack_head_indirect("@R13");
+
+          self.write_dec_sp();
+        }
         Pointer => unimplemented!(),
         Temp => unimplemented!(),
       },
